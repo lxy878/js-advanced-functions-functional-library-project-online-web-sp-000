c00e59339b333b30007a4341157e793dd3d52eda
Xiaoyi Liu <lxy878@gmail.com>
2020-09-04 16:50:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ const fi = (function() {
     uniq: function(array, isSorted, callback){
       const newArray = [array[0]];
       const cb = (e) => e;
-      console.log(cb(1))
+      // console.log(cb(1))
       if (isSorted){
         for(let i=1; i<array.length; i++){
           if (array[i-1] !== array[i]) {
